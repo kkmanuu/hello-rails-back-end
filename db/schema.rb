5788ba903ac699bdfd5615debcb38bfc93917c78
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-11-08 15:56:10 +0300

    update the migrations for greeting

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,19 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_231_107_141_049) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_07_141049) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'expenses', force: :cascade do |t|
-    t.text 'message'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "expenses", force: :cascade do |t|
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'greetings', force: :cascade do |t|
-    t.string 'message'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "greetings", force: :cascade do |t|
+    t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
